321f8ad5be19d43eb3521f39ee8a519421d7b460
Sahil071803 <sahilatram303@gmail.com>
2024-10-18 23:19:30 +0530

    Add this paragraph

---
+++ Switch.js
@@ -0,0 +1,16 @@
+let month = 3
+
+switch (month){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+        default :
+        console.log("No month is added");
+}
