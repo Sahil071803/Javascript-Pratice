5b46b9a23bd73cb56fc7d01d5902a91f61034e39
Sahil071803 <sahilatram303@gmail.com>
2024-10-20 00:38:49 +0530

    Add this paragraph

--- Switch.js
+++ Switch.js
@@ -14,3 +14,37 @@ switch (month){
         default :
         console.log("No month is added");
 }
+
+
+let marks = 75
+
+switch(marks){
+    case 1:
+    console.log("English : 80");
+    break;
+
+    case 2:
+    console.log("Physics : 85");
+    break;
+
+    case 3:
+    console.log("Chemistry : 75");
+    break;
+    
+
+    default:
+        console.log("Student is fail");
+        break;
+}
+
+
+let student = 4
+
+switch(student){
+    case 1:
+        console.log("Student")
+}
+
+
+
+
