b3fefd1ab0f5a633574be99bc247c39261167737
Sahil071803 <sahilatram303@gmail.com>
2024-10-20 01:08:20 +0530

    Add this paragraph

---
+++ For.js
@@ -0,0 +1,70 @@
+console.log("Sahil Atram");
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//     const element = i*i;
+//     console.log(element);
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//    console.log(`Outer value is: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`Inner value is ${j} and Outer Value is ${j} `);
+        
+//     }
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j <= 10; j++) {
+//        console.log(i + '*' + j + '=' + i*j);
+        
+//     }
+    
+// }
+// for (let i = 2; i < 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//        console.log(i + '*' + j + '=' + i*j);
+        
+//     }
+    
+// }
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(element);
+    
+}
+for (let i = 1; i <= 20; i++) {
+    const element = i;
+    console.log(element);
+    
+}
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`2 x ${i}: ${2*i}`);
+    
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`2 x ${i}: ${2*i*j}`);
+        
+//     }
+    
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`3 x ${i}: ${3*i}`);
+    
+}
+
+
